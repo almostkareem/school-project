088999de7417899c5130b2da23918d50e317cec8
capcashmere <capcashmere@gmail.com>
2018-12-29 21:23:44 +0100

    Identation and fixes

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const patient = require('./routes/patient.route'); // Imports routes for the patients
 const app = express();
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
 const mongoose = require('mongoose');
 let dev_db_url = 'mongodb://user:user123@ds125871.mlab.com:25871/nabili_db';
 let mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB, {useNewUrlParser: true});
+mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -16,9 +16,9 @@ app.use("/assets", express.static(__dirname + '/assets'));
 
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
